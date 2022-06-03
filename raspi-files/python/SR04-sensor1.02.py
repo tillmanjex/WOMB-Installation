8adbd1d4ce5ac3c04cdd2039f9038b59ec543223
tillmanRaspi <tillmanjex@mailbox.org>
2022-06-03 17:36:47 +0100

    continue on from before

--- raspi-files/python/SR04-sensor1.02.py
+++ raspi-files/python/SR04-sensor1.02.py
@@ -17,6 +17,7 @@ sensor = DistanceSensor(echo=24, trigger=21)
 def safe_exit(signum, frame):
 	exit(1)
 
+<<<<<<< Updated upstream
 def play_audio():
 	while sensor.distance < 0.2
 		pygame.mixer.music.play()
@@ -27,6 +28,30 @@ def read_distance():
 	while reading:
 		print("Distance: ", sensor.distance * 100)
 		sleep(0.1)	
+=======
+#def play_audio():
+	#if door_open:
+		#print("door open")
+
+	#if door_closed:
+		#print("door closed")
+	
+
+def read_distance():
+	while reading:
+		print("Distance: ", sensor.distance)
+		sleep(0.1)
+		#if sensor.distance > 0.2:
+			#pygame.mixer.Sound.play
+			
+			#door_open = True
+			#door_closed = False
+		#else:
+			#pygame.mixer.Sound.stop
+
+			#door_open = False
+			#door_closed = True
+>>>>>>> Stashed changes
 
 signal(SIGTERM, safe_exit)
 signal(SIGHUP, safe_exit)
@@ -35,9 +60,19 @@ try:
 	reader = Thread(target=read_distance, daemon=True)
 	reader.start()
 
+<<<<<<< Updated upstream
 	audio = Thread(target=play_audio, daemon=True)
 	audio.start()
+=======
+	#audio_player = Thread(target=play_audio, daemon=True)
+	#audio_player.start()
 
+	#if door_open:
+	#	print("door open")
+>>>>>>> Stashed changes
+
+	#if door_closed:
+	#	print("door closed")
 
 
 	pause()
