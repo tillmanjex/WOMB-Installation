70d903f2d82b46138ba464ee113bea9f36d8bdac
tillmanRaspi <tillmanjex@mailbox.org>
2022-06-03 16:37:45 +0100

    audio integration

--- raspi-files/python/SR04-sensor1.02.py
+++ raspi-files/python/SR04-sensor1.02.py
@@ -2,16 +2,26 @@
 # https://www.youtube.com/watch?v=JvQKZXCYMUM
 
 import RPi.GPIO as GPIO
+import pygame
 from threading import Thread
 from signal import signal, SIGTERM, SIGHUP, pause
 from time import sleep
 from gpiozero import DistanceSensor
+
+pygame.mixier.init()
+mygame.mixer.music.load("../audio/1khz.wav")
 
 reading = True
 sensor = DistanceSensor(echo=24, trigger=21)
 
 def safe_exit(signum, frame):
 	exit(1)
+
+def play_audio():
+	while sensor.distance < 0.2
+		pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+		continue
 
 def read_distance():
 	while reading:
@@ -24,6 +34,11 @@ signal(SIGHUP, safe_exit)
 try:
 	reader = Thread(target=read_distance, daemon=True)
 	reader.start()
+
+	audio = Thread(target=play_audio, daemon=True)
+	audio.start()
+
+
 
 	pause()
 
