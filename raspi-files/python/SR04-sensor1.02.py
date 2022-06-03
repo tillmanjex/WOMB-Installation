3a0a00ef36ab36d8395e0e539a6b02e0d433a271
tillmanRaspi <tillmanjex@mailbox.org>
2022-06-03 17:19:50 +0100

    pygame audio implementation

--- raspi-files/python/SR04-sensor1.02.py
+++ raspi-files/python/SR04-sensor1.02.py
@@ -8,8 +8,12 @@ from signal import signal, SIGTERM, SIGHUP, pause
 from time import sleep
 from gpiozero import DistanceSensor
 
-pygame.mixier.init()
-mygame.mixer.music.load("../audio/1khz.wav")
+pygame.mixer.pre_init(44100, 16, 2, 4096)
+pygame.mixer.init()
+pygame.mixer.Sound("raspi-files/audio/1khz.wav")
+
+door_open = True
+door_closed = False
 
 reading = True
 sensor = DistanceSensor(echo=24, trigger=21)
@@ -18,15 +22,23 @@ def safe_exit(signum, frame):
 	exit(1)
 
 def play_audio():
-	while sensor.distance < 0.2
-		pygame.mixer.music.play()
-	while pygame.mixer.music.get_busy() == True:
-		continue
+	if door_open:
+		print("door open")
+
+	if door_closed:
+		print("door closed")
+	
 
 def read_distance():
 	while reading:
-		print("Distance: ", sensor.distance * 100)
-		sleep(0.1)	
+		print("Distance: ", sensor.distance)
+		sleep(0.1)
+		while sensor.distance > 0.2:
+			door_open = True
+			door_closed = False
+		else:
+			door_open = False
+			door_closed = True
 
 signal(SIGTERM, safe_exit)
 signal(SIGHUP, safe_exit)
@@ -35,8 +47,8 @@ try:
 	reader = Thread(target=read_distance, daemon=True)
 	reader.start()
 
-	audio = Thread(target=play_audio, daemon=True)
-	audio.start()
+	audio_player = Thread(target=play_audio, daemon=True)
+	audio_player.start()
 
 
 
